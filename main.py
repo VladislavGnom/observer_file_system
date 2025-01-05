80fc6d77d0af899e095841bc9539816d851a1cad
VladislavGnom <130704830+VladislavGnom@users.noreply.github.com>
2025-01-05 22:23:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
 import os
+import signal
 import logging
 import shutil
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+
+interrupted = False    # run main loop
+
+
+class MyEventHandler(FileSystemEventHandler):
+    def __init__(self):
+        self.changes_item_lst = []    # keep path to changes files or directories
+    def on_created(self, event):
+        self.changes_item_lst.append(event.src_path)
+
+    def on_deleted(self, event):
+        try:
+            self.changes_item_lst.remove(event.src_path)
+        except ValueError:
+            ...
+
+    def on_moved(self, event):
+        current_path = event.src_path
+        new_path = event.dest_path
+        for path in changes_item_lst:
+            if current_path == path:
+                self.changes_item_lst.remove(current_path)
+                self.changes_item_lst.append(new_path)
+                break
+            
+
+def signal_handler(sig, frame):
+    global interrupted
+    logging.info('Получен сигнал завершения. Завершение работы...')
+    # Здесь можно добавить код для очистки или завершения работы
+    interrupted = True
+
+# Установка обработчика сигнала
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
 
 
 def delete_file(file_path: str) -> bool:
@@ -29,26 +66,7 @@ def delete_non_empty_directory(dir_path: str) -> bool:
 
 
 def main(path_name):
-    class MyEventHandler(FileSystemEventHandler):
-        def on_created(self, event):
-            changes_item_lst.append(event.src_path)
-
-        def on_deleted(self, event):
-            try:
-                changes_item_lst.remove(event.src_path)
-            except ValueError:
-                ...
-
-        def on_moved(self, event):
-            current_path = event.src_path
-            new_path = event.dest_path
-            for path in changes_item_lst:
-                if current_path == path:
-                    changes_item_lst.remove(current_path)
-                    changes_item_lst.append(new_path)
-                    break
-
-    changes_item_lst = []   # keep path to changes files or directories
+    global interrupted
     path = path_name
     event_handler = MyEventHandler()
     observer = Observer()
@@ -60,13 +78,16 @@ def main(path_name):
     observer.start()
     print('Programm started')
     try:
-        while True:
+        while not interrupted:
             time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
-    observer.join()
+    except Exception as error:
+        logging.error(error)
+    changes_item_lst = event_handler.changes_item_lst    # get list of paths
+    #observer.join()    # problem with it
     logging.info(f'{changes_item_lst=}')
-
+    
     while changes_item_lst:
         file_name = ''
         directory_name = ''
@@ -83,15 +104,17 @@ def main(path_name):
             result_statue = delete_file(current_path_to_obj)
         elif directory_name:
             result_statue = delete_non_empty_directory(current_path_to_obj)
-        changes_item_lst.remove(current_path_to_obj)
-        
+        try:
+            changes_item_lst.remove(current_path_to_obj)
+        except Exception as error:
+            logging.error(error)
 
 
 if __name__ == "__main__":
     path_name = "/home/vlad/Desktop"    # path to trackable directory
-    logging.basicConfig(filename=os.path.join(path_name, 'program.log'), filemode='w+', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(filename=os.path.join(path_name, 'program.log'), filemode='a+', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     print('Programm starting...')
-    logging.info('Programm starting...')
+    logging.info('\nProgramm starting...')
     main(path_name)
     print('Programm finished')
     logging.info('Programm finished')
