a533459465a76cb998365d577d03bdaad7339946
VladislavGnom <130704830+VladislavGnom@users.noreply.github.com>
2025-01-05 22:26:46 +0300

    Update start_script.sh

--- start_script.sh
+++ start_script.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+
+# Запуск Python-скрипта в фоновом режиме
 source venv/bin/activate
-python /home/vlad/main.py
+sudo sysctl fs.inotify.max_user_watches=1000000
+python /home/vlad/main.py &
+echo $! > pidfile
+
+# Функция, которая будет выполнена при завершении скрипта
+cleanup() {
+    echo "Завершение работы скрипта..."
+    # Имитация нажатия Ctrl+C
+    kill -SIGINT $(<pidfile)
+    echo $(<pidfile)
+    # Убедитесь, что процесс завершён
+    wait $(<pidfile)
+    echo "Скрипт завершён."
+}
+
+# Установка trap для перехвата сигнала SIGTERM
+trap cleanup SIGTERM
+
+echo $(<pidfile)
